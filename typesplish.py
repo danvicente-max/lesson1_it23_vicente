322bc4ad96d34b48ea7308b06a4cdb13f956f4ac
dwaekkie <ellplaz66@gmail.com>
2026-01-26 03:15:03 +0000

    Added the addition feature in calculator

--- typesplish.py
+++ typesplish.py
@@ -2,16 +2,6 @@ num1 = float(input("Enter first number: "))
 operator = input("Enter operator (+, -, *, /): ")
 num2 = float(input("Enter second number: "))
 
-if operator == "+":
-    print("Result:", num1 + num2)
-elif operator == "-":
-    print("Result:", num1 - num2)
-elif operator == "*":
-    print("Result:", num1 * num2)
-elif operator == "/":
-    if num2 != 0:
-        print("Result:", num1 / num2)
-    else:
-        print("Error: Division by zero!")
-else:
-    print("Invalid operator")
+if operator == +:
+	num1+num2
+elif operator == 
